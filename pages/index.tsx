51d8b35b1c2cca0d4cb53c596e14e0b77c163960
unknown <tuvalroz6@gmail.com>
2023-05-20 21:39:52 +0300

    get urls from mongo v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from '../lib/prisma'
-import { getVideoUrl } from "../mongo/mongo";
+import { getVideosUrl } from "../mongo/mongo";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const feed = await prisma.post.findMany({
@@ -19,52 +19,25 @@ export const getServerSideProps: GetServerSideProps = async () => {
     },
   });
 
-  /*let feedWithVideo = await feed.map(async (post) => {
-    if (post.hasVideo) {
-      const url = await getVideoUrl(post.id);
-      (post as PostProps).videoUrl = url;
-    }
-
-    return post;
-  });*/
-
-  let urlsMap = new Map<string, string>();
-
-
-
-
-  feed.forEach(async (post) => {
-    if (post.hasVideo) {
-      console.log("Before getVideoUrl");
-      const url = await getVideoUrl(post.id);
-      urlsMap.set(post.id.toString(), url);
-      console.log("After getVideoUrl222222 "+urlsMap.get(post.id.toString()));
-      console.log("sizeeee "+ urlsMap.size)
-    }
-  });
-
-  
-
-
-  //for each post get its video
+  const urls = await getVideosUrl();
 
   return {
-    props: { feed, urlsMap },
+    props: { feed, urls }
   };
 };
 
 type Props = {
   feed: PostProps[];
-  urlsMap: Map<string, string>;
+  urls: Map<number, string>;
 };
 
 const Blog: React.FC<Props> = (props) => {
-  console.log("size of map is "+ props.urlsMap.size)
+  console.log("size of urls is " + props.urls.size)
 
- 
+
 
   props.feed.forEach(post => {
-    let url = props.urlsMap.get(post.id.toString());
+    let url = props.urls.get(post.id);
     if (url) {
       post.videoUrl = url;
     }
