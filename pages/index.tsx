5417ef9e3be23653abe6ee0c945db2fd37cd5261
unknown <tuvalroz6@gmail.com>
2023-05-22 17:29:40 +0300

    change agian pageSize to 10

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { getVideosUrl } from "../mongo/mongo";
 import PageBar from "../components/PageBar";
 import { useOnlineStatus } from "../components/useOnline";
 
-const pageSize = 3;
+const pageSize = 10;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   let pageFromContext = parseInt(context.query?.page as string);
